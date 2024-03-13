f1abd2d00c6d50f6ae29a6d818414414bbff9262
Ilaria Roglieri <ilariaroglieri@gmail.com>
2024-03-13 18:17:51 +0100

    notebook articles: navi

--- navi.php
+++ navi.php
@@ -6,13 +6,13 @@
 
   <div class="container">
     <div class="navi d-flex d-column spacing-t-8 spacing-b-4">
-      <div class="full-width d-whole previous d-flex flex-row end">
-        <div class="d-three-twelfth t-half m-whole">
+      <div class="full-width d-whole  d-flex flex-row end">
+        <div class="dot-link previous d-three-twelfth t-half m-whole">
           <a class="s-large uppercase light underline" href="<?= get_permalink($circular_next->ID); ?>"><?= $circular_next->post_title; ?></a>
         </div>
       </div>
-      <div class="full-width d-whole next d-flex flex-row start spacing-t-4">
-        <div class="d-three-twelfth t-half m-whole">
+      <div class="full-width d-whole  d-flex flex-row start spacing-t-4">
+        <div class="dot-link next d-three-twelfth t-half m-whole">
           <a class="s-large uppercase light underline" href="<?= get_permalink($circular_prev->ID); ?>"><?= $circular_prev->post_title; ?></a>
         </div>
       </div>
