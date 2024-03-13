bc1df695d9dc36d8194eb0f9b03c99fceef9ca26
Ilaria Roglieri <ilariaroglieri@gmail.com>
2024-03-13 17:57:10 +0100

    notebook articles: navi

--- single.php
+++ single.php
@@ -185,7 +185,36 @@
         </div>
       <?php endif; ?>
 
-    </article>
+
+      <!-- article footer -->
+      <?php if( have_rows('bio_credits') ): ?>
+        <div id="article-footer" class="container spacing-t-4">
+          <div class="d-flex flex-row t-column">
+            <?php while( have_rows('bio_credits') ) : the_row();
+              $title = get_sub_field('module_title'); 
+              $text = get_sub_field('module_text');
+              $img = get_sub_field('module_image');
+            ?>
+
+            <div class="d-three-twelfth t-whole">
+              <p class="label uppercase light s-xxsmall"><?= $title; ?></p>
+
+              <div class="wywiwyg s-xxsmall spacing-t-1">
+                <?= $text; ?>
+              </div>
+
+              <?php if ($img): ?>
+                <img class="spacing-t-2" src="<?= $img['url']; ?>" />
+              <?php endif; ?>
+            </div>
+
+            <?php endwhile; ?>
+          </div>
+        </div>
+      <?php endif; ?>
+
+      <!-- navi -->
+      <?php include('navi.php'); ?>
 
   <?php endwhile; else: ?>
 
@@ -196,9 +225,5 @@
 
 </section>
 
-<div class="navigation">
-  <div class="navi previous"><?php previous_post_link( '%link','⟵', false ); ?></div>
-  <div class="navi next"><?php next_post_link( '%link','⟶', false ); ?></div>
-</div>
 
 <?php get_footer(); ?>
